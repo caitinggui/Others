11b46c6b899052f9475e39bc5d1ff63b4483c76b
caitinggui <1029645297@qq.com>
2017-11-27 17:41:17 +0800

    another method

--- attr_dict.py
+++ attr_dict.py
@@ -8,9 +8,24 @@ class AttrDict(dict):
         self.__dict__ = self
 
 
+class Row(dict):
+    """A dict that allows for object-like property access syntax."""
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
+
+
 if __name__ == '__main__':
     td = {'one': 1, 'two': 2, 'three': 3}
+    print "----- Row -----"
     # tda is still a dict
     tda = AttrDict(td)
     print tda
-    print assert td['one'] == tda.one
+    assert td['one'] == tda.one
+
+    tdb = Row(td)
+    print "----- Row -----"
+    print tdb
+    print tdb.one
