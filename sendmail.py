dafbdd2125ede0317a79949fee7f73e0783c3cea
caitinggui <1029645297@qq.com>
2017-12-11 17:51:21 +0800

    use qq smtp to email

--- sendmail.py
+++ sendmail.py
@@ -1,38 +1,25 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
+# coding=utf-8
 
 import smtplib
 from email.mime.text import MIMEText
-from email.header import Header
-
-sender = 'from@runoob.com'
-receivers = ['1029645297@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
-
-# 三个参数：第一个为文本内容，第二个 plain 设置文本格式，第三个 utf-8 设置编码
-message = MIMEText('Python 邮件发送测试...', 'plain', 'utf-8')
-message['From'] = Header("菜鸟教程", 'utf-8')
-message['To'] = Header("测试", 'utf-8')
-
-subject = 'Python SMTP 邮件测试'
-message['Subject'] = Header(subject, 'utf-8')
-
-
+msg_from = '1029645297@qq.com'  # 发送方邮箱
+passwd = 'dbumqtpqdakubebc'  # 填入发送方邮箱的授权码
+msg_to = '1029645297@qq.com'  # 收件人邮箱
+subject = "python邮件测试"  # 主题
+content = "这是我使用python smtplib及email模块发送的邮件"      # 正文
+msg = MIMEText(content)
+msg['Subject'] = subject
+msg['From'] = msg_from    # 邮件上显示的发件人，最好写成和实际发件人一致
+msg['To'] = msg_to    # 邮件上显示的收件人，最好写成和实际收件人一致
+print msg.as_string()
 try:
-    smtpObj = smtplib.SMTP('localhost')
-    smtpObj.sendmail(sender, receivers, message.as_string())
-    print "邮件发送成功"
-except smtplib.SMTPException:
-    print "Error: 无法发送邮件"
+    s = smtplib.SMTP_SSL("smtp.qq.com", 465)   # 邮件服务器及端口号
+    # s.set_debuglevel(1)
+    s.login(msg_from, passwd)
+    s.sendmail(msg_from, msg_to, msg.as_string())
+    print "发送成功"
+except s.SMTPException as e:
+    print "发送失败:", e
+finally:
+    s.quit()
 
-
-import smtplib
-s = smtplib.SMTP("localhost")
-tolist = ["1029645297@qehhjklqc.com", "caitinggui@qq.com", "caitinggui_whu@163.com"]
-msg = '''\
- ... From: Me@my.org
- ... Subject: testin'...
- ...
- ... This is a test '''
-r = s.sendmail("me@my.org", tolist, msg)
-print r
-s.quit()
