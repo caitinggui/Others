8e4b132c4c1e70a2b25aad43456490b2e4d03a8a
caitinggui <1029645297@qq.com>
2017-12-21 16:50:43 +0800

    use redis in web

--- redis-pool.py
+++ redis-pool.py
@@ -30,11 +30,11 @@ class RedisPool(object):
 
 if __name__ = "__main__":
     redis_config = {
-        "HOST": "192.168.10.219",
+        "HOST": "192.168.10.53",
         "PORT": 6002,
         "DB": 3,
         "MAX_CONNECTIONS": 20,
-        "PASSWORD": "Hhly2015"}
+        "PASSWORD": "password"}
     redis = RedisPool(redis_config)
     # 这里conn和redis.conn是等价的，用完之后都会被redispool 自动回收
     conn = redis.get_connection()
